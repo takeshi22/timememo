6bd4274ab0fb152660b7da3976374429934b4a6c
Takashi Hayakawa <chou_22@icloud.com>
2020-12-27 17:29:09 +0900

    cors追加

--- server/server.go
+++ server/server.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s password=%s", os.Getenv("USER_NAME"), os.Getenv("DB_NAME"), os.Getenv("PASSWORD")))
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:9000"}
+	r.Use(cors.New(config))
+	r.GET("/schedule", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "hello world!!",
 		})
 	})
 	r.Run(":5050")
