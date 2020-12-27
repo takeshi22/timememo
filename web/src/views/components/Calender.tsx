8a5f36ec1f3b54d9a47bb6b3dbabfce07b37070b
Takashi Hayakawa <chou_22@icloud.com>
2020-12-27 17:34:01 +0900

    月の変更処理の修正

--- web/src/views/components/Calender.tsx
+++ web/src/views/components/Calender.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { useState } from "react";
-// import "../style/style.scss";
+import { useEffect, useState } from "react";
 import { ListModal } from "./Modal/ListModal";
 import { initialData } from "../data/data";
 
@@ -18,6 +17,7 @@ export const UserCalender = () => {
 
   const [year, setYear] = useState(now.getFullYear());
   const [month, setMonth] = useState(now.getMonth() + 1);
+  
   const [isOpen, setIsOpen] = useState(false);
   const [registDate, setRegistDate] = useState("");
 
@@ -73,15 +73,33 @@ export const UserCalender = () => {
     return weeks.map((week) => <td key={week}>{week}</td>);
   };
 
-  const changeMonth = (direction: any) => {
+  const changeMonth = (direction: string) => {
     if (direction === "next") {
+      const nextMonth = month + 1;
+      if (nextMonth > 12) {
+        const newYear = year + 1;
+        setMonth(1);
+        setYear(newYear);
+        
+        return;
+      }
+
       setMonth(month + 1);
     } else if (direction === "prev") {
+      const prevMonth = month - 1;
+      if (prevMonth < 1) {
+        const prevYear = year - 1;
+        setMonth(12);
+        setYear(prevYear);
+
+        return;
+      }
+
       setMonth(month - 1);
     }
   };
 
-  const buttonRender = (icon: any, direction: any) => {
+  const buttonRender = (icon: string, direction: string) => {
     return <button onClick={() => changeMonth(direction)}>{icon}</button>;
   };
 
@@ -91,7 +109,7 @@ export const UserCalender = () => {
 
   return (
     <div className="content mx-auto">
-      <h2>{`${month}月`}</h2>
+      <h2>{`${year}年${month}月`}</h2>
 
       <div className="button">
         {buttonRender("<<", "prev")}
